d654e683c9d46119921ac0499a4134ca6800046e
178me <1403341393@qq.com>
2020-05-26 17:19:25 +0800

    updata

--- 合集/test3.js
+++ 合集/test3.js
@@ -1,149 +1,90 @@
 
+// if ($app.getAppName("moe.shizuku.privileged.api") == null) {
+//     alert("请下载Shizuku并按照说明连接电脑授权");
+//     $app.openUrl("https://www.coolapk.com/apk/moe.shizuku.privileged.api");
+//     exit();
+// }
+// $shell.setDefaultOptions({
+//     adb: false
+// });
 
+const user = "yzl178me";
+const pass = "Yangzelin995;";
 test();
 
 function test() {
 
     //邀请好友
-    // let t1 = id("close").findOne(3000);
+    // let t1 = id("close").findOne(1000);
     // t1.click();
     //我知道了
     // click("我知道了");
     // //   let t1 = text("关注").find().get(1);
     // Log(t1);
-    // if ($app.getAppName("moe.shizuku.privileged.api") == null) {
-    //     alert("请下载Shizuku并按照说明连接电脑授权");
-    //     $app.openUrl("https://www.coolapk.com/apk/moe.shizuku.privileged.api");
-    //     exit();
-    // }
-    // $shell.setDefaultOptions({
-    //     adb: false
-    // });
-    // Log($shell("input swipe 500 300 500 700 300"));
-    // let tt = className("android.widget.Image").depth(12).find().get(1).bounds();
-    // let x1 = tt.left * 1.09;
-    // let y1 = tt.top * 1.09;
-    // let x2 = tt.right * 0.83;
-    // let y2 = tt.bottom * 0.98;
-    // Log(x1);
-    // Log(y1);
-    // Log(x2);
-    // Log(y2); 
-    // Log(tt);
-    photo();
-    // var json = '{"key":"value","jian":"zhi"}';
-    // var obj = JSON.parse(json);
-
-    // var str = JSON.stringify(obj);
-    // console.log(str);
-    // let t = {
-    //     "data":{"res":"11,22|33,44"}
-    // }
-    // Log(t.data.res.split("|")[0].split(",")[0]);
-
+    Over_slider(user, pass)
 
 }
 
-function photo() {
+
+
+function Over_slider(usr, pass) {
+    /**
+     * 滑块验证
+     * 1. 此功能需要截图权限支持
+     * 2. 找到滑块验证区域范围
+     * 3. 找到积木控件范围
+     * 4. 截取滑块验证区域的图片
+     * 5. 对接联众平台
+     * 6. 计算滑动X坐标 X = 积木中间 + 两个积木之间的距离
+     * 7. 回收图片
+     */
     if (!requestScreenCapture()) {
-        toast("请求截图失败");
+        toast("过滑块需要截图权限支持");
         exit();
     };
-    let user = "yzl178me";
-    let pass = "Yang2zelin995;";
-    //获取截图
-    let p1 = images.captureScreen();
-    p1 = images.rotate(p1, 180);
-    Log(p1);
+
     //找到滑块区域控件
-    let tt = className("android.widget.Image").findOne(1000).bounds();
-    Log(tt);
-    //截取滑块验证区域
-    let p2 = images.clip(p1, tt.left, tt.top, tt.width(), tt.height());
-    Log(p2);
-    // images.save(p2, "/sdcard/test.png");
-    
-    //调用联众
-    // let x1 = parseInt(getCode(user, pass, p2).data.res.split("|")[0].split(",")[0]);
-    // let x2 = parseInt(getCode(user, pass, p2).data.res.split("|")[1].split(",")[1]);
-        // var lz = JSON.parse(getCode(user, pass, p2));
-        // Log(lz);
-    let x = getCode(user, pass, p2).;
-    // console.log(typeof str);
-    // Log(getCode(user, pass, p2));
-    // var test = getCode(user, pass, p2);
-    // Log(x1);
-    // Log(x2);
-    // swipe(160,500,x2,500,1500);
-    sleep(2000);
-    p1.recycle();
-    p2.recycle();
+    let M_1 = className("android.widget.Image").findOne(1000);
+    if (M_1) {
+        //找到滑块验证区域范围
+        M_1 = M_1.bounds();
+        Log(M_1);
+
+        // 找到积木控件范围
+        let M_2 = className("android.widget.Image").find().get(1).bounds();
+        Log(M_2);
+
+        //获取截图
+        let p1 = images.captureScreen();
+        p1 = images.rotate(p1, 180);
+        Log(p1);
+        //截取滑块验证区域
+        let p2 = images.clip(p1, M_1.left, M_1.top, M_1.width(), M_1.height());
+        Log(p2);
+        //对接联众
+        let res = getCode(user, pass, p2).data.res;
+        Log(res);
+
+        //计算X坐标
+        let x1 = parseInt(res.split("|")[0].split(",")[0]);
+        let x2 = parseInt(res.split("|")[1].split(",")[0]);
+        let x = M_2.centerX() + (x2 - x1);
+        Log(x);
+
+        //滑动滑块
+        swipe(160, 500, x, 500, 1000);
+
+        //回收图片
+        sleep(2000);
+        p1.recycle();
+        p2.recycle();
+
+    } else {
+        toastLog("没有找到滑块积木")
+    }
 
 }
 
-function getCode(username, password, img) {
-    http.__okhttp__.setTimeout(3e4);
-    var r = images.toBase64(img, format = "png"), i = device.release, c = device.model, s = device.buildId;
-    try {
-        var n = http.postJson("https://v2-api.jsdama.com/upload", {
-            softwareId: 19684,
-            softwareSecret: "LXgAa7NbYVCeVDsOXwqc01Hsqza5JIKYxVt5XNFS",
-            username: username,
-            password: password,
-            captchaData: r,
-            captchaType: 1310,
-            captchaMinLength: 0,
-            captchaMaxLength: 0,
-            workerTipsId: 0
-        }, {
-            headers: {
-                "User-Agent": "Mozilla/5.0 (Linux; Android " + i + "; " + c + " Build/" + s + "; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 Mobile Safari/537.36",
-            }
-        });
-    } catch (e) {
-        return {
-            code: "-1",
-            msg: "网络链接超时...",
-            data: {}
-        };
-    }
-    var d = n.body.json(), p = d.code, m = d.message;
-    if ("10079009" == p) return {
-        code: p,
-        msg: m,
-        data: {}
-    };
-    if ("10142006" == p) return {
-        code: p,
-        msg: m,
-        data: {}
-    };
-    if ("10142004" == p) return {
-        code: p,
-        msg: m,
-        data: {}
-    };
-    if ("10142005" == p) return {
-        code: p,
-        msg: m,
-        data: {}
-    };
-    if ("10079006" == p) return {
-        code: p,
-        msg: m,
-        data: {}
-    };
-    if ("0" == p) {
-        return {
-            code: p,
-            msg: m,
-            data: {
-                res: d.data.recognition
-            }
-        };
-    }
-    return d;
-}
 
 
 function Log(obj) {
