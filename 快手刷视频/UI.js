c28781c1bf9a71a33770e42e04be0dcc1e952d0f
evanmeek <the_lty_mail@foxmail.com>
2020-05-26 16:55:10 +0800

    update

--- 快手刷视频/UI.js
+++ 快手刷视频/UI.js
@@ -2,52 +2,48 @@
 
 var menu_color = "#000000";
 ui.layoutFile("./mainUI.xml");
-ui.viewpager.setTitles(["功能管理","功能参数"]);
+ui.viewpager.setTitles(["功能管理", "功能参数"]);
 ui.tabs.setupWithViewPager(ui.viewpager);
 activity.setSupportActionBar(ui.toolbar);
 ui.toolbar.setupWithDrawer(ui.drawer);
-ui.emitter.on("create_options_menu", menu => {
-    menu.add("日志");
-    menu.add("关于");
+ui.emitter.on("create_options_menu", (menu) => {
+  menu.add("日志");
+  menu.add("关于");
 });
-ui.emitter.on("options_item_selected", (e,item) => {
-    switch(item.getTitle()){
+ui.emitter.on("options_item_selected", (e, item) => {
+  switch (item.getTitle()) {
     case "日志":
-        app.startActivity('console');
-        break;
-    }
+      app.startActivity("console");
+      break;
+  }
 });
 // 设置左滑菜单栏的数据源
 ui.menu.setDataSource([
-    {
-        text: "One",
-        icon: "@drawable/ic_android_black_48dp"
-    },
-    {
-        text: "Two",
-        icon: "@drawable/ic_settings_black_48dp"
-    },
-    {
-        text: "Three",
-        icon: "@drawable/ic_favorite_black_48dp"
-    },
-    {
-        text: "Exit",
-        icon: "@drawable/ic_exit_to_app_black_48dp"
-    }
+  {
+    text: "One",
+    icon: "@drawable/ic_android_black_48dp",
+  },
+  {
+    text: "Two",
+    icon: "@drawable/ic_settings_black_48dp",
+  },
+  {
+    text: "Three",
+    icon: "@drawable/ic_favorite_black_48dp",
+  },
+  {
+    text: "Exit",
+    icon: "@drawable/ic_exit_to_app_black_48dp",
+  },
 ]);
 
-ui.swBtn.on("click", ()=>{
-    log("被点击");
-    if(!ui.swBtn.isChecked())
-        ui.vw.attr("bg","#f44336");
-    else
-        ui.vw.attr("bg","#4caf50");
+ui.swBtn.on("click", () => {
+  log("被点击");
+  if (!ui.swBtn.isChecked()) ui.vw.attr("bg", "#f44336");
+  else ui.vw.attr("bg", "#4caf50");
 });
-ui.swBtn2.on("click", ()=>{
-    log("被点击");
-    if(!ui.swBtn2.isChecked())
-        ui.vw2.attr("bg","#f44336");
-    else
-        ui.vw2.attr("bg","#4caf50");
+ui.swBtn2.on("click", () => {
+  log("被点击");
+  if (!ui.swBtn2.isChecked()) ui.vw2.attr("bg", "#f44336");
+  else ui.vw2.attr("bg", "#4caf50");
 });
